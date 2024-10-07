d46b65fadcd374ef3debd1245112c88fd9a3b6e2
marswallet <saheedsuleiman4955@gmail.com>
2024-10-07 22:16:43 +0100

    mars

--- admin001/config.php
+++ admin001/config.php
@@ -1,9 +1,9 @@
 <?php
 // Database credentials
-define('DB_HOST', 'localhost');        // Replace with your database host, usually 'localhost'
-define('DB_USERNAME', 'root'); // Replace with your MySQL username
-define('DB_PASSWORD', ''); // Replace with your MySQL password
-define('DB_NAME', 'bronasir');     // Replace with your database name
+define('DB_HOST', 'sql12.freemysqlhosting.net');        // Replace with your database host, usually 'localhost'
+define('DB_USERNAME', 'sql12736087'); // Replace with your MySQL username
+define('DB_PASSWORD', '8GDyAJUThG'); // Replace with your MySQL password
+define('DB_NAME', 'sql12736087');     // Replace with your database name
 
 // Create a connection
 $conn = mysqli_connect(DB_HOST, DB_USERNAME, DB_PASSWORD, DB_NAME);
